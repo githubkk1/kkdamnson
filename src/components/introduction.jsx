5fe50fa128d28b01039f54408a0062ea062c5d0f
Karun Krishna <karunkrishnakk@gmail.com>
2021-04-19 21:45:43 +0530

    Made Bg and also remember we have problem changing the font color in landing?

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -7,7 +7,7 @@ export default class Introduction extends Component {
         <section id="colorlib-hero" className="js-fullheight" data-section="home">{/*Confused here because changing it makes the home look different*/}
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              <li>
+            <li style={{backgroundImage: 'url(images/img_bg1.jpg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -15,7 +15,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
                           <h1>Hi! <br />I'm KK</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1att-0yOxBlDxsRMhixi1Ga9aaakA7iBA/view?usp=sharing" target="_blank" rel="noopener noreferrer">Click here for my bio-data   <i className="icon-download4" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1att-0yOxBlDxsRMhixi1Ga9aaakA7iBA/view?usp=sharing" target="_blank" rel="noopener noreferrer">Click here for my bio-data<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
@@ -23,36 +23,7 @@ export default class Introduction extends Component {
                 </div>
               </li>
               </ul>
-              <li>
-                <div className="overlay" />
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner">
-                        <div className="desc">
-                          <h1>I love building<br /> THINGS !!</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1att-0yOxBlDxsRMhixi1Ga9aaakA7iBA/view?usp=sharing" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-download4" /></a></p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div className="overlay" />
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner">
-                        <div className="desc">
-                          <h1>I often <br/>Write ... </h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://dhruvbarochiya.me/" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </li>
+
           </div>
         </section>
       </div>
